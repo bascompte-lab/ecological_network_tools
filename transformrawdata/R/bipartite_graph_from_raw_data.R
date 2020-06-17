5770d9e954d5da277b9ad44f04abbf9a9a117e68
vindigna <vindigna@ethz.ch>
2020-06-17 14:51:32 +0200

    fixed dependences

--- transformrawdata/R/bipartite_graph_from_raw_data.R
+++ transformrawdata/R/bipartite_graph_from_raw_data.R
@@ -1,5 +1,6 @@
 bipartite_graph_from_raw_data = function(data){
   require(igraph)
+  require(dplyr)
   net = incidence_matrix_from_raw_data(data) %>% graph_from_incidence_matrix(.)
   return(net)
 }
